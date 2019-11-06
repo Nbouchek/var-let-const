2fc4b418c065d1398e6609e7b87c35513e646d71
Nacer Bouchekhima <nbouchek@gmail.com>
2019-11-06 11:17:07 +0000

    the "use strict" mode - demo

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
-let name = "Nacer";
+// let name = "Nacer";
+//
+// function greet() {
+//   let age = 25;
+//   let name = "Linda";
+//   console.log(name, age);
+// }
+//
+// greet();
+"use strict";
 
-function greet() {
-  let age = 25;
-  let name = "Linda";
-  console.log(name, age);
-}
-
-greet();
+userName = "Nacer";
+console.log(userName);
